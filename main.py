bf505465c4b5b8dc08ed348cdbef7ca896d56d5d
Alex Laws <tal1286@gmail.com>
2018-11-20 19:56:50 -0800

    Pig Latin Fact

--- main.py
+++ main.py
@@ -19,10 +19,17 @@ def get_fact():
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact().strip()
+    payload = {'input_text': fact}
+
+    r = requests.post('http://hidden-journey-62459.herokuapp.com/piglatinize/',
+                      allow_redirects=False,
+                      data=payload)
+    url = r.headers['Location']
+
+    return "<a href='{}'>{}</a>".format(url, url)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
